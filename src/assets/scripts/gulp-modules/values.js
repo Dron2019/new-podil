e62f70417e1be0b6a56794e1e1707b07d8d1062d
Dron2019 <mr.andrey@bigmir.net>
2020-08-13 12:50:07 +0300

    advantages add

---
+++ src/assets/scripts/gulp-modules/values.js
@@ -0,0 +1,16 @@
+/* beautify preserve:start */
+@@include('gsap.js');
+/* beautify preserve:end */
+
+function getHeight(e, target) {
+    return -target.getBoundingClientRect().height;
+}
+let timeline = gsap.timeline();
+
+timeline.from('.front-block__bg', 2, { ease: "power4.in", scale: 1.2 }).
+from('.fron-block__text', 2, { background: 'rgba(23, 152, 213, 0.0)' }, '<')
+    .from('.fron-block__text svg path', 1, {
+        autoAlpha: 0,
+        y: getHeight,
+    })
+    .from('.fron-block__text div', 1, { autoAlpha: 0 })
