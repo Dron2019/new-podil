708770baffc27fc82fabc3d895782754bcd3d6b5
Dron2019 <mr.andrey@bigmir.net>
2020-08-13 09:51:57 +0300

    location finish, values page daa

--- src/assets/scripts/gulp-modules/home-libs.js
+++ src/assets/scripts/gulp-modules/home-libs.js
@@ -5,5 +5,6 @@
 @@include('../libs/scroll-magic/plugins/debug.addIndicators.min.js')
 @@include('../libs/scroll-magic/plugins/animation.gsap.min.js')
 @@include('../libs/scrollify/scrollify.js')
+@@include('../libs/count-up/countup.min.js')
 /* @@include('../libs/artem-scroll/scroll.js')*/
 /* beautify preserve:end */
--- src/assets/scripts/gulp-modules/location-google-map.js
+++ src/assets/scripts/gulp-modules/location-google-map.js
@@ -15,20 +15,29 @@ setTimeout(func, 1000);
 
 function initMap() {
     var gmarkers1 = [];
-    filterMarkers = function(category) {
-        for (i = 0; i < gmarkers1.length; i++) {
-            marker = gmarkers1[i];
+    filterMarkers = function(category, categoriesArray) {
+        // for (i = 0; i < gmarkers1.length; i++) {
+        //     marker = gmarkers1[i];
+        //     var markerMain = gmarkers1.find(item => item.category === 'main');
 
-            var markerMain = gmarkers1.find(item => item.category === 'main');
+        //     if (marker.category == category || category == 'all') {
+        //         marker.setMap(map);
+        //         markerMain.setMap(map);
+        //     } else {
+        //         marker.setMap(null);
+        //         markerMain.setMap(map);
+        //     }
+        // }
 
-            if (marker.category == category || category == 'all') {
-                marker.setMap(map);
-                markerMain.setMap(map);
+        gmarkers1.forEach(el => {
+            if (categoriesArray.has(el.category)) {
+                el.setMap(map);
+                el.setAnimation(google.maps.Animation.DROP);
             } else {
-                marker.setMap(null);
-                markerMain.setMap(map);
+
+                el.setMap(null);
             }
-        }
+        })
     };
 
 
@@ -84,6 +93,8 @@ function initMap() {
 
     });
 
+    /** Кастомизированые попап (на будущее) */
+    // customizedPopup(map)
 
 
     var polygonCoords = [
@@ -109,33 +120,53 @@ function initMap() {
     //Добавляем на карту
     polygon.setMap(map);
 
-    var markers_spritesheet;
-
-    if (document.location.pathname === '/') {
-        markers_spritesheet = 'img/main_page_map_markers_spritesheet.png';
-    } else {
-        markers_spritesheet = '../img/main_page_map_markers_spritesheet.png';
-    }
-
     // var baseFolder = '/wp-content/themes/rusaniv/assets/images/markers/';
     var baseFolder = './assets/images/markers/';
     var defaultMarkerSize = new google.maps.Size(94, 75),
         buildLogoSize = new google.maps.Size(94, 75);
     var markersAdresses = {
-            main: `${baseFolder}marker-main.svg`,
-            sport: `${baseFolder}sport-complex.svg`,
-            // kinder: `${baseFolder}marker-bear.svg`,
-            // meal: `${baseFolder}marker-meal.svg`,
-            // park: `${baseFolder}marker-park.svg`,
-            // shop: `${baseFolder}marker-shop.svg`,
-            // work: `${baseFolder}marker-work.svg`,
-            // school: `${baseFolder}marker-school.svg`,
-            // sale: `${baseFolder}marker-sale.svg`,
+        main: `${baseFolder}marker-main.svg`,
+        sport: `${baseFolder}marker-sport-complex.svg`,
+        school: `${baseFolder}marker-school.svg`,
+        bank: `${baseFolder}marker-bank.svg`,
+        kindergarden: `${baseFolder}marker-kindergarden.svg`,
+        park: `${baseFolder}marker-park.svg`,
+        meal: `${baseFolder}marker-meal.svg`,
+        dentist: `${baseFolder}marker-dentist.svg`,
+        medicine: `${baseFolder}marker-medicine.svg`,
+        metro: `${baseFolder}marker-metro.svg`,
+        post: `${baseFolder}marker-post.svg`,
+        shop: `${baseFolder}marker-shop.svg`,
+        supermarkets: `${baseFolder}marker-supermarkets.svg`,
+    };
+    let markerPopupStyle = `
+        style="
+        background: #1798D5;
+        padding:5px 10px;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 22px;"
+        `;
+    /**Елементы, при клике на который будет происходить фильтрация */
+    let filterItems = document.querySelectorAll('.legend-js li');
+    /**Массив, куда записываются выбраные категории*/
+    let choosedCategories = new Set();
+    choosedCategories.add('main');
+    filterItems.forEach(item => {
+        item.addEventListener('click', () => {
+            item.classList.toggle('active');
+            item.classList.contains('active') ?
+                choosedCategories.add(item.dataset.category) :
+                choosedCategories.delete(item.dataset.category);
+            filterMarkers('main', choosedCategories);
+        });
+    })
 
 
 
-        }
-        /* beautify preserve:start */
+
+
+    /* beautify preserve:start */
     var markersData = [
         {
             content: `<img style="background:white" src="${markersAdresses.main}">`,
@@ -143,7 +174,30 @@ function initMap() {
             type: 'main',
             icon: { url: markersAdresses.main,scaledSize: buildLogoSize,}
         },
-
+        {
+            content: `<div ${markerPopupStyle}>PARK</div>`,
+            position: { lat: 50.478303,lng: 30.516779},
+            type: 'park',
+            icon: { url: markersAdresses.park,scaledSize: buildLogoSize,}
+        },
+        {
+            content: `<div ${markerPopupStyle}>Shop</div>`,
+            position: { lat: 50.480303,lng: 30.516779},
+            type: 'shop',
+            icon: { url: markersAdresses.shop,scaledSize: buildLogoSize,}
+        },
+        {
+            content: `<div ${markerPopupStyle}>Post</div>`,
+            position: { lat: 50.482303,lng: 30.516779},
+            type: 'post',
+            icon: { url: markersAdresses.post,scaledSize: buildLogoSize,}
+        },
+        {
+            content: `<div ${markerPopupStyle}>Bank</div>`,
+            position: { lat: 50.484303,lng: 30.516779},
+            type: 'bank',
+            icon: { url: markersAdresses.bank,scaledSize: buildLogoSize,}
+        },
     ]
 /* beautify preserve:end */
     var infowindow = new google.maps.InfoWindow({
@@ -154,7 +208,6 @@ function initMap() {
 
 
 
-    /**Вывод карты со всеми маркерами на странице Инфраструктуры и С одним маркером на остальных страницах */
     markersData.forEach(function(marker) {
         var markerSettings = {};
         var category = marker.type;
@@ -168,13 +221,72 @@ function initMap() {
         google.maps.event.addListener(mapMarker, 'click', function() {
             infowindow.setContent(marker.content);
             infowindow.open(map, mapMarker);
-            console.log(mapMarker);
             map.panTo(this.getPosition());
         });
         mapMarker.name = marker.type;
         gmarkers1.push(mapMarker);
-
     });
-
 };
+
+
+// function customizedPopup(map) {
+
+
+//     /**
+//      * A customized popup on the map.
+//      */
+//     class Popup extends google.maps.OverlayView {
+//         constructor(position, content) {
+//                 super();
+//                 this.position = position;
+//                 content.classList.add("popup-bubble");
+//                 // This zero-height div is positioned at the bottom of the bubble.
+//                 const bubbleAnchor = document.createElement("div");
+//                 bubbleAnchor.classList.add("popup-bubble-anchor");
+//                 bubbleAnchor.appendChild(content);
+//                 // This zero-height div is positioned at the bottom of the tip.
+//                 this.containerDiv = document.createElement("div");
+//                 this.containerDiv.classList.add("popup-container");
+//                 this.containerDiv.appendChild(bubbleAnchor);
+//                 // Optionally stop clicks, etc., from bubbling up to the map.
+//                 Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+//             }
+//             /** Called when the popup is added to the map. */
+//         onAdd() {
+
+//                 this.getPanes().floatPane.appendChild(this.containerDiv);
+//             }
+//             /** Called when the popup is removed from the map. */
+//         onRemove() {
+//                 if (this.containerDiv.parentElement) {
+//                     this.containerDiv.parentElement.removeChild(this.containerDiv);
+//                 }
+//             }
+//             /** Called each frame when the popup needs to draw itself. */
+//         draw() {
+//             const divPosition = this.getProjection().fromLatLngToDivPixel(
+//                 this.position
+//             );
+//             // Hide the popup when it is far out of view.
+//             const display =
+//                 Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+//                 "block" :
+//                 "none";
+
+//             if (display === "block") {
+//                 this.containerDiv.style.left = divPosition.x + "px";
+//                 this.containerDiv.style.top = divPosition.y + "px";
+//             }
+
+//             if (this.containerDiv.style.display !== display) {
+//                 this.containerDiv.style.display = display;
+//             }
+//         }
+//     }
+//     popup = new Popup(
+//         new google.maps.LatLng(50.478303, 30.516779),
+//         document.getElementById("content")
+//     );
+//     popup.setMap(map);
+// }
 // Google map end
