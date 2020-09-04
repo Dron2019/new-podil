f6f7577d289f6bcf9c878d40ae9f0b1ab21759f8
Dron2019 <mr.andrey@bigmir.net>
2020-09-04 16:05:21 +0300

    pvavki v1 completed + 1

--- src/assets/scripts/gulp-modules/about.js
+++ src/assets/scripts/gulp-modules/about.js
@@ -6,18 +6,11 @@
 ;;;
 @@include('../libs/artem-scroll/scroll.js')
 ;;;;;;
-@@include('../../../../node_modules/svg-path-parser/parser.js')
+
 
 ;;;
 /* beautify preserve:end */
-var d = `M3,7 5-6 L1,7 1e2-.4 m-10,10 l10,0  
-  V27 89 H23           v10 h10             
-  C33,43 38,47 43,47   c0,5 5,10 10,10     
-  S63,67 63,67         s-10,10 10,10       
-  Q50,50 73,57         q20,-5 0,-10        
-  T70,40               t0,-15              
-  A5,5 45 1,0 40,20    a5,5 20 0,1 -10-10  Z`;
-console.log(parseSVG(d));
+
 /**Imported GSAP END */
 
 /**Displacment Hover */
