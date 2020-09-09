aacae4c5d48febfedf3e5a2121ad8255722a6823
Dron2019 <mr.andrey@bigmir.net>
2020-09-09 15:29:37 +0300

    under-construction page add

--- src/assets/scripts/gulp-modules/forms-handler.js
+++ src/assets/scripts/gulp-modules/forms-handler.js
@@ -1,6 +1,10 @@
 /*Form handler */
 let submitList = document.querySelectorAll('.submit-js');
 const SEND_URL = './static/application.php';
+const WP_ACTION = {
+    keyname: 'action',
+    keyValue: 'app',
+};
 const loadIcon = `
 <svg class="load-indication" style="position:absolute; bottom:20px; right:20px; /*transform:translate(-100%,-100%);*/" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="20px" height="20px" viewBox="0 0 128 128" xml:space="preserve"><path fill="#000000" fill-opacity="1" id="ball1" class="cls-1" d="M67.712,108.82a10.121,10.121,0,1,1-1.26,14.258A10.121,10.121,0,0,1,67.712,108.82Z"><animateTransform attributeName="transform" type="rotate" values="0 64 64;4 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;" dur="800ms" repeatCount="indefinite"></animateTransform></path><path fill="#000000" fill-opacity="1" id="ball2" class="cls-1" d="M51.864,106.715a10.125,10.125,0,1,1-8.031,11.855A10.125,10.125,0,0,1,51.864,106.715Z"><animateTransform attributeName="transform" type="rotate" values="0 64 64;10 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;0 64 64;" dur="800ms" repeatCount="indefinite"></animateTransform></path><path fill="#000000" fill-opacity="1" id="ball3" class="cls-1" d="M33.649,97.646a10.121,10.121,0,1,1-11.872,8A10.121,10.121,0,0,1,33.649,97.646Z"><animateTransform attributeName="transform" type="rotate" values="0 64 64;20 64 64;40 64 64;65 64 64;85 64 64;100 64 64;120 64 64;140 64 64;160 64 64;185 64 64;215 64 64;255 64 64;300 64 64;" dur="800ms" repeatCount="indefinite"></animateTransform></path></svg>
 `;
@@ -47,6 +51,7 @@ function send(object, url, form) {
     for (const key in object) {
         data.append(key, object[key]);
     }
+    data.append(WP_ACTION.keyname, WP_ACTION.keyValue);
     loadIndication(form, loadIcon, 'on');
 
 
@@ -56,6 +61,7 @@ function send(object, url, form) {
     }).catch(res => {
         console.log(res);
         sendMessageStatus(form, 'Помилка відправки');
+        loadIndication(form, loadIcon, 'off');
         //console.log(data);
     }).then(res => {
         return res.text();
